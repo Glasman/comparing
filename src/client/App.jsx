3560a4297dd66c3dc0dd0ab65394c82d18cb8a01
markglasman <glasman.mark@gmail.com>
2025-05-19 14:57:39 -0400

    succesfully added SingleItem.jsx and added react router route

--- src/client/App.jsx
+++ src/client/App.jsx
@@ -3,11 +3,13 @@ import axios from "axios";
 import { Routes, Route } from "react-router-dom";
 import "./App.css";
 import AllItems from "./Components/AllItems";
+import SingleItem from "./Components/SingleItem";
 
 function App() {
   return <div className="App">
     <Routes>
       <Route path="/" element={<AllItems/>}/>
+      <Route path="/:id" element={<SingleItem/>}/>
     </Routes>
   </div>;
 }
