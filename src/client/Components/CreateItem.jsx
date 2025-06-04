2325c147f903267fe0653b8067aa4c89cb1e7996
markglasman <glasman.mark@gmail.com>
2025-06-04 12:30:11 -0400

    initiated CreateItem.jsx

---
+++ src/client/Components/CreateItem.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+function CreateItem() {
+  return (
+    <div>
+        <h2>Add your own items to compare!</h2>
+        <div>
+            <input placeholder="Name" value={} onChange={}/>
+            <input placeholder="Image Url" value={} onChange={}/>
+            <input placeholder="Description" value={} onChange={}/>
+            <input placeholder="Category" value={} onChange={}/>
+        </div>
+    </div>
+  )
+}
+
+export default CreateItem
