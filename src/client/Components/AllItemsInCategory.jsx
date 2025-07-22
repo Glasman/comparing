9c0f343faedfdd92f8a16b41ab69cdda195a8db0
markglasman <glasman.mark@gmail.com>
2025-07-22 15:21:04 -0400

    updated AllItemsInCategroy so that a category description appears at the top, added linkes for cards

--- src/client/Components/AllItemsInCategory.jsx
+++ src/client/Components/AllItemsInCategory.jsx
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from "react";
 import axios from "axios";
 import { useParams } from "react-router-dom";
+import { Link } from "react-router-dom";
 
 function AllItemsInCategory() {
   const [items, setItems] = useState([]);
@@ -20,6 +21,7 @@ function AllItemsInCategory() {
       }
     };
     getItems();
+    console.log(items);
   }, [category]);
 
   useEffect(() => {
@@ -28,14 +30,17 @@ function AllItemsInCategory() {
 
   return (
     <div>
+      <h2>{items[0]?.category_description}</h2>
       {items.map((item) => (
-        <div
-          key={item.id}
-          style={{ border: "2px solid black", width: "500px" }}
-        >
-          <h2>{item.name}</h2>
-          <img src={item.image_url} />
-        </div>
+        <Link to={`/item/${item.id}`}>
+          <div
+            key={item.id}
+            style={{ border: "2px solid black", width: "500px" }}
+          >
+            <h2>{item.name}</h2>
+            <img src={item.image_url} />
+          </div>
+        </Link>
       ))}
     </div>
   );
